4fd46622e4b9227ec1571e91b792d388de3a66a7
masdmtr <masdmtr@gmail.com>
2018-03-05 04:33:55 +0300

    Initial commit

---
+++ src/main/java/org/skillsmonster/skilledbot/SkilledBotApplication.java
@@ -0,0 +1,12 @@
+package org.skillsmonster.skilledbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkilledBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkilledBotApplication.class, args);
+	}
+}
